56bc01ab660e848b32653ede73f5299997f03004
Qi Wang <qiwan@qiwang-virtual-machine.(none)>
2012-10-27 11:06:46 -0400

    second part of fraction assignment with subclasses

--- sbtPractice/src/main/scala/Fraction.scala
+++ sbtPractice/src/main/scala/Fraction.scala
@@ -5,67 +5,46 @@ import annotation.tailrec
  * User: qiwang
  * Date: 9/25/12
  * Time: 6:32 PM
- * Fraction operations for positional number system
+ * Fraction operations for both positional number system and native language number system
  */
-trait BaseNum {
-//  type U <: {
-    def +[U<:Any](x: U): U
-    def *[U<:Any](x: U): U
-//  }
-}
-
-//trait PNNum extends BaseNum{
-//  type U = PN
-//}
-//
-//trait IntNum extends BaseNum{
-//  type U = Int
-//}
-
-abstract class Fraction[U<:{
-  def +[M<:Any](x: M): M
-  def *[M<:Any](x: M): M
-}] {
-//  this: BaseNum =>
+abstract class Fraction[U](val n: U, val d: U) {
   type T <: Fraction[U]
-  val n: U
-  val d: U
 
-  def makeInstance(x: U, y: U): T
+  def +(r: T): T
 
-  def +(r: T): T = makeInstance(n * r.d + r.n * d, d * r.d)
-  def -(r:T):T
-  def *(r: T): T = makeInstance(n * r.n, d * r.d)
-  def /(r:T):T
+  def -(r: T): T
+
+  def *(r: T): T
+
+  def /(r: T): T
+
+  def equals(r: T): Boolean
+  def simplify: T
+
+  override def toString = n + "/" + d
 }
 
-class PNFraction(val n: PN, val d: PN) extends Fraction[PN]{
-
-  import ImplicitConversion._
-
+class PNFraction(override val n: PN, override val d: PN) extends Fraction(n, d) {
   require(!(d equals 0))
 
   type T = PNFraction
-//  override type U = PN
-
-  def makeInstance(x: PN, y: PN): T = new PNFraction(x, y).simplify
 
   def this(n: PN) = this(n, PN(List(1)))
 
-    def +(r:T):T = {
-      super.+(r).simplify
-    }
-
-  def -(r: PNFraction): PNFraction = {
-    Fraction(n * r.d - r.n * d, d * r.d).simplify
+  def +(r: T): T = {
+    new PNFraction(n * r.d + r.n * d, d * r.d).simplify
   }
 
-    def *(r:T):PNFraction = {
-      Fraction(n*r.n,d*r.d).simplify
-    }
+  def -(r: PNFraction): PNFraction = {
+    new PNFraction(n * r.d - r.n * d, d * r.d).simplify
+  }
+
+  def *(r: T): PNFraction = {
+    new PNFraction(n * r.n, d * r.d).simplify
+  }
 
   def /(r: PNFraction): PNFraction = {
-    Fraction(n * r.d, d * r.n).simplify
+    new PNFraction(n * r.d, d * r.n).simplify
   }
 
   def equals(f: PNFraction): Boolean = ((n equals f.n) && (d equals f.d))
@@ -76,39 +55,33 @@ class PNFraction(val n: PN, val d: PN) extends Fraction[PN]{
   }
 
   @tailrec
-  private def gcd(x: PN, y: PN): PN = {
+  final def gcd(x: PN, y: PN): PN = {
     if (y equals PN(List(0))) x else gcd(y, x % y)
   }
-
-  override def toString = n + "/" + d
 }
 
 
-class NativeFraction(val n: Int, val d: Int) extends Fraction[Int]{
-
+class NativeFraction(override val n: Int, override val d: Int) extends Fraction(n, d) {
   require(!(d equals 0))
 
-  override type T = NativeFraction
-//  override type U = Int
+  type T = NativeFraction
 
   def this(n: Int) = this(n, 1)
 
-  def makeInstance(x: Int, y: Int): T = new NativeFraction(x, y).simplify
-
   override def +(r: NativeFraction): NativeFraction = {
-    Fraction(n*r.d+r.n*d,d*r.d).simplify
+    new NativeFraction(n * r.d + r.n * d, d * r.d).simplify
   }
 
   override def -(r: NativeFraction): NativeFraction = {
-    Fraction(n * r.d - r.n * d, d * r.d).simplify
+    new NativeFraction(n * r.d - r.n * d, d * r.d).simplify
   }
 
   override def *(r: NativeFraction): NativeFraction = {
-    Fraction(n * r.n, d * r.d).simplify
+    new NativeFraction(n * r.n, d * r.d).simplify
   }
 
   override def /(r: NativeFraction): NativeFraction = {
-    Fraction(n * r.d, d * r.n).simplify
+    new NativeFraction(n * r.d, d * r.n).simplify
   }
 
   def equals(f: NativeFraction): Boolean = ((n equals f.n) && (d equals f.d))
@@ -119,16 +92,12 @@ class NativeFraction(val n: Int, val d: Int) extends Fraction[Int]{
   }
 
   @tailrec
-  private def gcd(x: Int, y: Int): Int = {
+  final def gcd(x: Int, y: Int): Int = {
     if (y == 0) x else gcd(y, x % y)
   }
-
-  override def toString = n + "/" + d
 }
 
 object Fraction {
-  //  def apply(n:PN) = new PNFraction(n)
-
   def apply(n: PN, d: PN) = {
     new PNFraction(n, d).simplify
   }
